99c6336332b805da801dbf6c8abe2b4fa0d29920
Antoine Rey <antoine.rey@backmarket.com>
2023-12-07 12:22:35 +0100

    Feat: Initial commit

---
+++ src/utils.js
@@ -0,0 +1,20 @@
+/**
+ * Add two numbers, and return the result.
+ *
+ * @param {Number} a
+ * @param {Number} b
+ * @return {Number}
+ */
+function add(a, b) {
+  return a + b
+}
+
+/**
+ * Reverse a string, and return the result.
+ *
+ * @param {String} input
+ * @return {String}
+ */
+function reverse(input) {
+  return [...input].reverse()
+}
